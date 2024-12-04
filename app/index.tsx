af8417a98693698b1a61cce31435036b316b86c8
IngridAkeida <ingridsanchesp@gmail.com>
2024-12-04 16:31:19 +0100

    Update layout and enhance home screen with logo and styling

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,12 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
+      <Image source={require('../assets/sources/logo.png')} style={styles.logo} resizeMode="cover"/>
       <View>
-        <Text> Hello!! </Text>
+        <Text style={styles.h1}> Ingrid's Store</Text>
+        <Text style={styles.h2}> IHere you can find everything you want!</Text>
       </View>
     </SafeAreaView>
   );
@@ -15,5 +17,21 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  logo:{
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  h1: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  h2: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10,
   },
 });
