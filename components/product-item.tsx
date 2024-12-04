0a9d7be9c9d9f2ef778a9eb0b1885b2c6fcc1b02
IngridAkeida <ingridsanchesp@gmail.com>
2024-12-04 17:45:44 +0100

    Enhance ProductItem component with navigation and improved layout; add image and styling

--- components/product-item.tsx
+++ components/product-item.tsx
@@ -1,5 +1,6 @@
-import { Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import { Product } from "../types/product";
+import { Link } from "expo-router";
 
 type props = {
   data: Product;
@@ -7,8 +8,30 @@ type props = {
 
 export default function ProductItem({ data } : props) {
   return (
-    <View>
-      <Text>Product {data.title} - {data.price}</Text>
-    </View>
+    <Link href={`/product/${data.id}`} asChild>
+      <Pressable style={styles.container} >
+        <Image 
+          style={styles.image} 
+          source={{ uri: data.image }}
+          resizeMode="cover"
+        />
+        <View>
+          <Text>{data.title}</Text>
+          <Text>{data.price}</Text>
+        </View>
+      </Pressable>
+    </Link>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 24,
+  }
+});
