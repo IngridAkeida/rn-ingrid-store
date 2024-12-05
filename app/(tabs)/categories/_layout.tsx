d92c6267139330ad2ad645c8716e06e91779ada7
IngridAkeida <ingridsanchesp@gmail.com>
2024-12-05 10:23:28 +0100

    Update CategoryLayout to remove old category screen and add dynamic category screen

--- app/(tabs)/categories/_layout.tsx
+++ app/(tabs)/categories/_layout.tsx
@@ -4,7 +4,7 @@ export default function CategoryLayout() {
   return (
     <Stack>
       <Stack.Screen name="list" options={{ title: 'Categories' }}/>
-      {/* <Stack.Screen name="category" options={{ title: 'Category' }}/> */}
+      <Stack.Screen name="[id]"/>
     </Stack>
   );
 }
