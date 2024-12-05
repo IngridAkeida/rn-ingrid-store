9318606a1ba8595d208bfd1233be5c1eb5273e78
IngridAkeida <ingridsanchesp@gmail.com>
2024-12-05 11:06:40 +0100

    Add product screen layout with dynamic title and scrollable content

--- app/product/[id].tsx
+++ app/product/[id].tsx
@@ -1,9 +1,19 @@
-import { StyleSheet } from "react-native";
+import { Stack } from "expo-router";
+import { ScrollView, StyleSheet } from "react-native";
 import { SafeAreaView, Text, View } from "react-native";
 
 export default function Screen() {
   return (
     <SafeAreaView style={styles.container}>
+      <Stack.Screen options={{ title: '' }} />
+      <ScrollView style={styles.area}>
+        <View>
+          <Text>Product</Text>
+        </View>
+      </ScrollView>
+      <View style={styles.buttonArea}>
+        <Text>Button</Text>
+      </View>
 
     </SafeAreaView>
   );
@@ -14,5 +24,13 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  area: {
+    flex: 1,
+    width: '100%',
+  },
+  buttonArea: {
+    width: '100%',
+    padding: 10,
   },
 });
